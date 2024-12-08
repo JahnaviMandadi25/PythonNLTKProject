6a53c635f8dea715d2fded6cf54ec95c0ed451cc
JahnaviMandadi25 <jahnaviapplications@gmail.com>
2024-12-08 13:20:37 -0500

    Added logic for stop words

--- tokenization.py
+++ tokenization.py
@@ -2,9 +2,10 @@
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from docx import Document
-
+from nltk.corpus import stopwords
 # Download the necessary NLTK tokenizer models
 nltk.download("punkt")
+
 
 # Load the Word document
 doc = Document('/Users/jahnavimandadi/Library/Mobile Documents/com~apple~CloudDocs/Documents/Depression In Animals/Investigating Emotional Distress Across Species.docx')
@@ -23,3 +24,14 @@ sent_tokenized = sent_tokenize(story)
 # Print the results of tokenization
 print("---- Word tokenize ----\n", word_tokenized)
 print("\n---- Sentence tokenize ----\n", sent_tokenized)
+nltk.download('stopwords')
+
+stop_words = stopwords.words('english')
+
+filtered_words = []
+
+for word in word_tokenized:
+    if word.casefold() not in stop_words:
+        filtered_words.append(word)
+
+print('The filtered words are:', filtered_words)
