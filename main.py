8ce6daab33de4feb5fad75561457b53dd7d51bc3
JahnaviMandadi25 <jahnaviapplications@gmail.com>
2024-12-08 14:16:28 -0500

    Added logic for stop words

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from docx import Document
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 # Download the necessary NLTK tokenizer models
 nltk.download("punkt")
-
+# Download the part-of-speech tagger from NLTK
+nltk.download('averaged_perceptron_tagger')
+# Download the WordNet resource, used by the lemmatizer
+nltk.download('wordnet')
 
 # Load the Word document
 doc = Document('/Users/jahnavimandadi/Library/Mobile Documents/com~apple~CloudDocs/Documents/Depression In Animals/Investigating Emotional Distress Across Species.docx')
@@ -29,9 +36,59 @@ nltk.download('stopwords')
 stop_words = stopwords.words('english')
 
 filtered_words = []
+# Create a list of words from 'story' that are not stopwords
+filtered_list = [word for word in word_tokenize(story) if word.casefold() not in stop_words]
 
-for word in word_tokenized:
-    if word.casefold() not in stop_words:
-        filtered_words.append(word)
+# Display the filtered list
+print('list of words:',filtered_list)
 
-print('The filtered words are:', filtered_words)
+stemmer = PorterStemmer()
+
+stemmed_words = [stemmer.stem(word) for word in filtered_list]
+
+print('list of stemmed words:',stemmed_words)
+
+tagged_words = nltk.pos_tag(filtered_list)
+print('POS Tagged Words:', tagged_words)
+
+
+# Create an instance of the WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# Apply lemmatization to each word in the list of stemmed words
+lemmatized_words = [lemmatizer.lemmatize(word) for word in stemmed_words]
+
+# Display the lemmatized words
+print('Lemmatized words:',lemmatized_words)
+
+fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(18,5),
+                        gridspec_kw={"height_ratios": [1],
+                                     "hspace": 0.7})
+fdist = nltk.FreqDist(stemmed_words)
+top_30_words = fdist.most_common(30)
+axs.bar([word[0] for word in top_30_words], [word[1] for word in top_30_words])
+for i in range(len(top_30_words)):
+    axs.text(i, top_30_words[i][1], str(top_30_words[i][1]))
+axs.set_xticklabels([word[0] for word in top_30_words], rotation=45)
+axs.set_title("Top 30 Words")
+axs.set_xlabel("Word")
+axs.set_ylabel("Count of Words")
+plt.show()
+
+# Create another subplot for the word cloud
+fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(10,20),
+                        gridspec_kw={"height_ratios": [1],
+                                     "hspace": 0.7})
+
+# Generate a word cloud from the frequency distribution of words
+wordcloud = WordCloud(width = 800, height = 300,
+                background_color ="white",
+                stopwords = stopwords.words("english"),
+                min_font_size = 10).generate_from_frequencies(fdist)
+
+# Configure the plot settings for the word cloud
+plt.figure(figsize = (8, 8), facecolor = None)
+axs.imshow(wordcloud)
+axs.axis("off")
+axs.set_title("Word Cloud-Size indicates frequency")
+plt.show()
